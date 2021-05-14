7922c68fb3e822d22d308ef1269eefc7dd08ec3a
DannyPadron09 <danny.padron09@gmail.com>
2021-05-14 12:28:15 -0400

    square function tested and working properly

--- PointerByReference.c
+++ PointerByReference.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void square(int *xPointer);
+void square(int * const xPointer);
 
 
 int main()
 {
+    int num = 9;
 
+    square(&num);
+    printf("The square of the given number is %d\n", num);
     return 0;
+
 }
 
-void square(int *xPointer)
+void square(int * const xPointer)
 {
     // *xPointer - dereferences the pointer and gets the value
     *xPointer = (*xPointer) * (*xPointer); // dereferences and multiplies by itself
